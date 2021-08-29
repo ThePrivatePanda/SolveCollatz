b26e95e9687e173ff4cdec5fd2ff407189864660
mittal-notahaxer <party.mittal@gmail.com>
2021-08-29 22:59:38 +0530

    added a readme and clean!

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bruteforce solving the Collatz conjecture
+Solving the collatz conjecture by brute force
+
+## What am I
+A script aimed to solve each number for the collatz conjecture by brute force
+
+# Purpose of my life
+The solve purpose of this script's life is to learn about increasing efficiency.\n
+Suggestions are welcomed :)
+
+# Contact
+You can contact me on discord on `Chliste#9712`
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
+from os import pipe
 import time
-def get_nums():
-    with open("nums.txt") as file:
-        return file.readlines()
 
-def write_num(num):
-    with open("nums.txt", "a") as file:
-        file.write(num)
+stl = []
+try:
+    idk = time.time()
+    for i in range(1, 111111):
+        x = i
 
-ab = time.time()
-for i in range(1, 101):
-    get_nums()
-print(time.time()-ab)
-
-cd = time.time()
-for i in range(1, 101):
-    write_num(f"\n{i}")
-print(time.time()-cd)
-
-print(get_nums())
-ef = time.time()
-for i in range(1, 101):
-    skem = f"{i}\n"
-    print(f"{skem in get_nums()}")
-print(time.time()-ef)
-
-
-idk = time.time()
-for i in range(1, 25000):
-    before = time.time()
-
-    x = i
-    stl = []
-    print(x)
-
-    while x != 4 and x not in get_nums():
-        if x % 2 == 0:
-            x = x/2
-        else:
-            x = (3*x)+1
-        stl.append(x)
-    print(time.time()-before)
-
-print(time.time()-idk)
+        while x != 4 and x not in stl:
+            if x % 2 == 0:
+                x = x/2
+            else:
+                x = (3*x)+1
+            if x not in stl:
+                stl.append(x)
+except KeyboardInterrupt as e:
+    print(stl)
+    print(i)
+    print(time.time()-idk)
+    quit()
