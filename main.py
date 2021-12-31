ee02502606c838e41ea6f73d4f781a99e5b64ff5
koala9712 <86164003+koala9712@users.noreply.github.com>
2021-12-31 21:22:19 +0530

    Better

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ try:
     for i in range(1, 111111):
         x = i
 
-        while x != 4 and x not in stl:
+        while x not in stl:
             if x % 2 == 0:
                 x = x/2
             else:
                 x = (3*x)+1
             if x not in stl:
                 stl.append(x)
-except KeyboardInterrupt as e:
+except KeyboardInterrupt:
     print(stl)
     print(i)
     print(time.time()-idk)
-    quit()
+    raise SystemExit
